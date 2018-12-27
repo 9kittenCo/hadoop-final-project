0c3936219dda3d504772b262c572eb2275a821fe
Viacheslav <vyachik@gmail.com>
2018-12-27 14:09:05 +0200

    clean comments

--- udf_ip/src/main/scala/com/nykytenko/IpToGeo.scala
+++ udf_ip/src/main/scala/com/nykytenko/IpToGeo.scala
@@ -9,33 +9,12 @@ import org.apache.hadoop.hive.serde2.objectinspector.{ObjectInspector, ObjectIns
 import org.apache.hadoop.io.{BooleanWritable, Text}
 
 class IpToGeo extends UDF {
-//  class IpToGeo extends GenericUDF {
-//  var converters = Array.empty[ObjectInspectorConverters.Converter]
-
-//  override def initialize(arguments: Array[ObjectInspector]): ObjectInspector = {
-//    if (arguments.length != 2) {
-//      throw new UDFArgumentLengthException("IpToGeo accepts exactly two arguments.")
-//    }
-//
-//    converters = arguments.map { arg =>
-//      ObjectInspectorConverters.getConverter(arg, PrimitiveObjectInspectorFactory.javaStringObjectInspector)
-//    }
-//
-//    PrimitiveObjectInspectorFactory.javaBooleanObjectInspector //(PrimitiveObjectInspector.PrimitiveCategory.BOOLEAN)
-//  }
-
-//  override def evaluate(arguments: Array[DeferredObject]): BooleanWritable = {
   def evaluate(ip: Text, subnet: Text): Boolean = {
-//    val ip: String     = converters(0).convert(arguments(0).get()).toString
-//    val subnet: String = converters(1).convert(arguments(1).get()).toString
     val address = ip.toString
     val cidr    = subnet.toString
-//    new BooleanWritable(isInRange(address, cidr))
     isInRange(address, cidr)
   }
-
-//  override def getDisplayString(children: Array[String]): String = s"country(${children(0)}, ${children(1)})"
-
+  
   def isInRange(ip: String, subnet: String):Boolean = {
     if (ip.nonEmpty || subnet.nonEmpty) {
       val subnetUtils = new SubnetUtils(subnet)
